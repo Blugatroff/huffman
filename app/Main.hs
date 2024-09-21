c2a1f104382d3d1b53bdc89f0861b08f69d594b1
Jasper Reef <mail@jasperreef.com>
2024-09-21 15:02:43 +0200

    optimized

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,37 @@
 module Main (main) where
 
+import Control.Concurrent (threadDelay)
+import Control.Exception (bracket)
+import Control.Monad (when)
+import Control.Monad.ST.Strict (runST)
 import Data.Bits (Bits (shiftR), shiftL, (.&.), (.|.))
+import Data.ByteString (StrictByteString)
+import Data.ByteString qualified as BSS
 import Data.ByteString.Builder qualified as BS
+import Data.ByteString.Internal qualified as BSSI
 import Data.ByteString.Lazy (ByteString)
 import Data.ByteString.Lazy qualified as BS
+import Data.ByteString.Unsafe qualified as BSSU
+import Data.Data (Proxy (Proxy))
+import Data.Foldable (for_)
 import Data.Function (on)
+import Data.Functor (void)
+import Data.IORef (modifyIORef', newIORef, readIORef, writeIORef)
 import Data.List (foldl')
 import Data.Map.Strict qualified as M
 import Data.Maybe (fromMaybe)
 import Data.PQueue.Min qualified as PQ
+import Data.Primitive (Prim, sizeOf)
+import Data.Vector qualified as Vector
+import Data.Vector.Unboxed.Mutable qualified as MVector
 import Data.Word (Word64, Word8)
-import Debug.Trace (trace, traceM)
-import GHC.Base (Word8#)
+import Debug.Trace (trace, traceM, traceShowId)
+import Foreign (Storable (poke), plusPtr)
+import GHC.Base (Int (I#), Int#, Word64#, Word8#, andI#, iShiftL#, iShiftRL#, orI#, (+#), (-#), (/=#), (<=#), (>=#))
 import System.Environment (getArgs)
 import System.Exit (exitSuccess)
+import System.IO (IOMode (..), hClose, hFileSize, hPrint, openFile, stderr, withFile)
+import System.IO.Unsafe (unsafeDupablePerformIO, unsafePerformIO)
 
 type Bit = Word8
 
@@ -33,7 +51,13 @@ byteToBits b = [1 .&. (b `shiftR` i) | i <- [7, 6 .. 0]]
 
 bitsToByte :: [Bit] -> Byte
 bitsToByte bits =
-  foldl' (.|.) 0 [fromIntegral (fromEnum b) `shiftL` i | (i, b) <- zip [7, 6 .. 0] bits]
+  foldl' (.|.) 0 [b `shiftL` i | (i, b) <- zip [7, 6 .. 0] bits]
+
+bitsToWord :: forall a. (Prim a) => (Num a) => (Bits a) => [Bit] -> a
+bitsToWord bits =
+  foldl' (.|.) 0 $ [fromIntegral b `shiftL` i | (i, b) <- zip (reverse [0 .. typeSize - 1]) bits]
+ where
+  typeSize = 8 * sizeOf (0 :: a)
 
 fromBits :: [Bit] -> BS.Builder
 fromBits bits = case splitAt 8 bits of
@@ -81,42 +105,101 @@ deserialize bits cursor = do
 
 type Byte = Word8
 
-readTree :: ByteString -> Node
+readTree :: ByteString -> Maybe Node
 readTree input = buildTree queue
  where
-  queue = PQ.fromList $ uncurry Leaf <$> M.toList occurences
+  queue = runST $ do
+    occurences <- MVector.new 256
+    for_ (BS.toChunks input) $ \chunk -> do
+      let loop !i | i >= BSS.length chunk = pure ()
+          loop i = do
+            let index = fromIntegral $ BSS.index chunk i
+            !count <- MVector.read occurences index
+            MVector.write occurences index (count + 1)
+            loop (i + 1)
+      loop 0
+    PQ.fromList . map (uncurry Leaf) . filter ((0 /=) . snd) . zip [0 ..] <$> MVector.foldr (:) [] occurences
 
   buildTree q
-    | PQ.null q = undefined
+    | PQ.null q = Nothing
   buildTree q = do
     let (left, q') = PQ.deleteFindMin q
     if PQ.null q'
-      then left
+      then Just left
       else do
         let (right, q'') = PQ.deleteFindMin q'
         buildTree $ PQ.insert (Branch left right) q''
 
-  bytes = BS.unpack input
-
-  occurences :: M.Map Byte Int
-  occurences =
-    foldl' (flip $ M.alter (Just . (+ 1) . fromMaybe 0)) M.empty bytes
-
-encode :: Node -> ByteString -> ByteString
-encode tree input =
-  BS.toLazyByteString $
-    mconcat
-      [ BS.int64LE (BS.length input)
-      , fromBits (serialize tree)
-      , fromBits (concatMap translateByte (BS.unpack input))
-      ]
+encode :: Node -> Word64 -> ByteString -> ByteString
+encode tree inputLength input = header <> body''
  where
-  translateByte :: Byte -> [Bit]
-  translateByte byte = fromMaybe (error "translateByte failed") $ M.lookup byte lookupMap
+  header = BS.toLazyByteString (mconcat [BS.word64LE inputLength, serializedTree])
+  body = BS.toLazyByteString (fromBits (concatMap translateByte (BS.unpack input)))
+  serializedTree = fromBits (serialize tree)
 
-  lookupMap :: M.Map Byte [Bit]
-  lookupMap = go tree
+  body' = BS.unfoldr nextByte (BS.unpack input, 0, 0)
+
+  body'' = BS.fromChunks $ chunks input
+
+  chunks :: ByteString -> [StrictByteString]
+  chunks input = go (BS.toChunks input) 0# 0#
    where
+    max :: Int# -> Int# -> Int#
+    max a b = case a >=# b of
+      0# -> b
+      _ -> a
+    go :: [StrictByteString] -> Int# -> Int# -> [StrictByteString]
+    go [] _ 0# = []
+    go [] bits n = BSS.singleton (fromIntegral (I# bits `shiftR` 56)) : go [] (bits `iShiftL#` 8#) (max 0# (n -# 8#))
+    go (chunk : rest) bits nBits = do
+      let (encodedChunk, nextEncodedChunks) = BSSI.unsafeCreateUptoN' (BSS.length chunk * 2) $ \ptr -> do
+            let lopi :: Int -> Int -> Int# -> Int# -> IO (Int, [StrictByteString])
+                lopi !outLen index bits nBits | index >= BSS.length chunk = pure (outLen, go rest bits nBits)
+                lopi outLen index bits nBits = case nBits <=# 8# of
+                  1# -> do
+                    let byte = BSSU.unsafeIndex chunk index
+                    let !(I# nextBits, I# nNextBits) = (Vector.!) lookupMap'' $ fromIntegral byte
+                    lopi outLen (index + 1) (orI# bits (nextBits `iShiftRL#` nBits)) (nBits +# nNextBits)
+                  _ -> do
+                    let (byte :: Word8) = fromIntegral $ I# bits `shiftR` 56
+                    poke (plusPtr ptr outLen) byte
+                    lopi (outLen + 1) index (bits `iShiftL#` 8#) (nBits -# 8#)
+            lopi 0 0 bits nBits
+      encodedChunk : nextEncodedChunks
+
+  nextByte :: ([Word8], Word64, Int) -> Maybe (Word8, ([Word8], Word64, Int))
+  nextByte ([], _, 0) = Nothing
+  nextByte ([], bits, n) = Just (fromIntegral (bits `shiftR` 56), ([], bits `shiftL` 8, max 0 (n - 8)))
+  nextByte (rest, bits, nBits) | nBits >= 8 = Just (fromIntegral (bits `shiftR` 56), (rest, bits `shiftL` 8, nBits - 8))
+  nextByte (byte : rest, bits, nBits) = do
+    let (nextBits, nNextBits) = (Vector.!) lookupMap' $ fromIntegral byte
+    nextByte (rest, bits .|. (nextBits `shiftR` nBits), nBits + nNextBits)
+
+  translateByte :: Byte -> [Bit]
+  translateByte byte = (Vector.!) lookupMap $ fromIntegral byte
+
+  lookupMap'' :: Vector.Vector (Int, Int)
+  lookupMap'' = Vector.generate 256 $ \i -> do
+    let bits = fromMaybe undefined $ M.lookup (fromIntegral i) map
+    (bitsToWord bits, length bits)
+   where
+    map = go tree
+    go (Leaf byte _) = M.singleton byte [] :: M.Map Byte [Bit]
+    go (Branch left right) = ((0 :) <$> go left) <> ((1 :) <$> go right)
+
+  lookupMap' :: Vector.Vector (Word64, Int)
+  lookupMap' = Vector.generate 256 $ \i -> do
+    let bits = fromMaybe undefined $ M.lookup (fromIntegral i) map
+    (bitsToWord bits, length bits)
+   where
+    map = go tree
+    go (Leaf byte _) = M.singleton byte [] :: M.Map Byte [Bit]
+    go (Branch left right) = ((0 :) <$> go left) <> ((1 :) <$> go right)
+
+  lookupMap :: Vector.Vector [Bit]
+  lookupMap = Vector.generate 256 $ \i -> fromMaybe undefined $ M.lookup (fromIntegral i) map
+   where
+    map = go tree
     go (Leaf byte _) = M.singleton byte [] :: M.Map Byte [Bit]
     go (Branch left right) = ((0 :) <$> go left) <> ((1 :) <$> go right)
 
@@ -124,16 +207,15 @@ decode :: ByteString -> Maybe ByteString
 decode input | BS.length input < 8 = Nothing
 decode input = do
   let (messageByteLen :: Word64) = foldl' (.|.) 0 [fromIntegral (BS.index input i) `shiftL` (fromIntegral i * 8) | i <- [0 .. 7]]
-  let bits = newBitVec $ BS.drop 8 input
-  (tree, body) <- deserialize bits 0
-  let go cursor _ _ | cursor >= bits.len = []
+  (tree, bodyOffset) <- deserialize (newBitVec $ BS.drop 8 input) 0
+  let body = concatMap byteToBits $ BS.unpack $ BS.drop (8 + fromIntegral (bodyOffset + 7) `div` 8) input
+  let go [] _ _ = []
       go _ _ i | i >= messageByteLen = []
-      go cursor node i = case node of
+      go bits@(bit : rest) node i = case node of
         Branch left right -> do
-          let bit = getBit bits cursor
-          go (cursor + 1) (case bit of 0 -> left; 1 -> right) i
+          go rest (case bit of 0 -> left; 1 -> right) i
         Leaf byte _ ->
-          byte:go cursor tree (i + 1)
+          byte : go bits tree (i + 1)
   Just $ BS.pack $ go body tree 0
 
 main :: IO ()
@@ -145,7 +227,12 @@ main = do
   args <- getArgs
   if "encode" `elem` args
     then do
-      tree <- readTree <$> BS.readFile (head (tail args))
-      encoded <- encode tree <$> BS.readFile (head (tail args))
+      let filepath = head $ tail args
+      (tree, filesize) <- withFile filepath ReadMode $ \file -> do
+        filesize :: Word64 <- fromIntegral <$> hFileSize file
+        !tree <- readTree <$> BS.hGetContents file
+        pure (tree, filesize)
+      encoded <- encode (fromMaybe undefined tree) filesize <$> BS.readFile (head (tail args))
       BS.putStr encoded
-    else BS.interact $ fromMaybe "" <$> decode
+    else -- print tree
+      BS.interact $ fromMaybe "" <$> decode
